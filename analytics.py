40843e7602da48aa34337080db33e2d7723a80dd
d4ndres <d4ndres@gmail.com>
2025-01-21 18:08:10 -0500

    estandarizando representacion de gragficas

--- analytics.py
+++ analytics.py
@@ -1,7 +1,43 @@
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+def load_data(file_paths):
+    return [pd.read_csv(path) for path in file_paths]
+
+def plot_and_save(pdf, data, x, y, title, xlabel=None, ylabel=None, labels=None, colors=None, markers=None):
+    xlabel = xlabel if xlabel else x
+    plt.figure(figsize=(12, 6))
+    for i, y_col in enumerate(y):
+        plt.plot(data[x], data[y_col], 
+                 label=labels[i] if labels else y_col, 
+                 color=colors[i] if colors else None, 
+                 marker=markers[i] if markers else None)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel if ylabel else f"f({xlabel})")
+    plt.legend()
+    plt.grid(True)
+    pdf.savefig()
+    plt.close()
+
+
+PLOT_CONFIG = {
+    "figsize": (12, 6),
+    "xlabel": "Tiempo (s)",
+    "grid": True
+}
+
+
+
+
+
+
 
 def create_main_file():
-    base_name = "analitycs"
+    base_name = "vehicle_analysis"
+
     output_dir = "output"
     last_index = 0
 
@@ -15,15 +51,46 @@ def create_main_file():
             if last_index is None or index > last_index:
                 last_index = index
 
-    while True:
-        last_index += 1
-        file_name = f"{base_name}_{last_index}"
-        file_path = os.path.join(output_dir, file_name)
 
-        # Code to create the file
-        with open(file_path, 'w') as file:
-            file.write("")
+    last_index += 1
+    file_name = f"{base_name}_{last_index}.pdf"
+    file_path = os.path.join(output_dir, file_name)
 
-        break
+    # Code to create the file
+    FILE_PATHS = {
+        "general": "./VSL_Demo/General.csv",
+        "wheels": "./VSL_Demo/Wheels.csv",
+        "engine": "./VSL_Demo/Engine.csv"
+    }
+
+    file_paths = [FILE_PATHS["general"], FILE_PATHS["wheels"], FILE_PATHS["engine"]]
+    general_data, wheels_data, engine_data = load_data(file_paths)
+
+    plots = [
+        {
+            "data": general_data, 
+            "x": "time", 
+            "y": ["throttle", "brake", "clutch"], 
+            "title": "Pedales: Acelerador, Freno y Clutch", 
+            "labels": ["Acelerador", "Freno", "Clutch"], 
+            "markers": [".", "o", "x"]
+        },
+        {
+            "data": general_data, 
+            "x": "time", 
+            "y": ["velocity (m/s)"], 
+            "title": "Velocidad del Vehículo (m/s)", 
+            "labels": ["Velocidad (m/s)"], 
+            "colors": ["orange"], 
+            "markers": ["o"]
+        },
+    ]
+
+
+    with PdfPages(file_path) as pdf:
+        for plot in plots:
+            plot_and_save(pdf, **plot)
+
+    print(f"Archivo PDF generado: {file_path}")
 
 create_main_file()  
